370aaa9cabe12d633de632061059baaa05ba1a8f
Mehul Mohan <mehulmpt@gmail.com>
2021-02-09 01:42:49 +0530

    lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,23 @@
 import React from 'react'
-import './App.css'
+import Form from './Form'
+import FilterButton from './FilterButton'
 import Todo from './Todo'
+import './App.css'
 
-const App = (props) => {
+function App(props) {
 	const taskList = props.tasks.map((task) => (
-		<Todo id={task.id} name={task.name} completed={task.completed} />
+		<Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
 	))
-
 	return (
 		<div className="todoapp stack-large">
-			<h1>TodoMatic</h1>
-			<form>
-				<h2 className="label-wrapper">
-					<label htmlFor="new-todo-input" className="label__lg">
-						What needs to be done?
-					</label>
-				</h2>
-				<input type="text" className="input input__lg" autoComplete="off" />
-				<button type="submit" className="btn btn__primary btn__lg">
-					Add
-				</button>
-			</form>
+			<Form />
 			<div className="filters btn-group stack-exception">
-				<button type="button" className="btn toggle-btn">
-					all
-				</button>
-				<button type="button" className="btn toggle-btn">
-					Active
-				</button>
-				<button type="button" className="btn toggle-btn">
-					Completed
-				</button>
+				<FilterButton />
+				<FilterButton />
+				<FilterButton />
 			</div>
 			<h2 id="list-heading">3 tasks remaining</h2>
-			<ul
-				role="list"
-				className="todo-list stack-large stack-exception"
-				aria-labelledby="list-heading"
-			>
+			<ul role="list" className="todo-list stack-large stack-exception">
 				{taskList}
 			</ul>
 		</div>
---
+++ src/FilterButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FilterButton(props) {
+	return (
+		<button type="button" className="btn toggle-btn">
+			<span>all </span>
+		</button>
+	)
+}
+
+export default FilterButton
